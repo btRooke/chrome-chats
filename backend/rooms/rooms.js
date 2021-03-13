0f5f798ee41c6523cec3b58b333354a000809e65
Sean Patterson <sap22@st-andrews.ac.uk>
2021-03-13 22:28:27 +0000

    Image storing and loading in database

--- backend/rooms/rooms.js
+++ backend/rooms/rooms.js
@@ -17,7 +17,11 @@ class Room {
     }
 
     addMessage(username, payload) {
-        db.sendMessage(this.hash, {'username': username, 'message': payload})
+        db.addMessage(this.hash, {'username': username, 'message': payload});
+    }
+
+    addImage(username, payload) {
+        db.addImage(this.hash, {'username': username, 'message': payload, 'isImage': true});
     }
 }
 
@@ -40,19 +44,25 @@ function joinRoom(io, socket) {
         }
 
         let room = ROOMS[urlID];
-
         room.addUser(data.username);
 
         console.log(`Joined Room`)
+        socket.emit('joined-room', {room: room.url});
 
-        socket.emit("joined-room", {room: room.url});
         sendMessage(io, socket, room);
+        sendImage(io, socket, room);
     });
 }
 
 function sendMessage(io, socket, room) {
     socket.on('send-message', (data) => {
         room.addMessage(data.username, data.message);
+    });
+}
+
+function sendImage(io, socket, room, username) {
+    socket.on('send-image', (data) => {
+        room.addImage(username, data.message);
     });
 }
 
