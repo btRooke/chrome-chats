e345623af54856d5b5573160ad04d1bea4f55c24
Tim Beatham <beatham.tim@gmail.com>
2021-03-13 14:14:27 +0000

    Http

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,13 @@
 const admin = require('firebase-admin');
 
 var serviceAccount = require("./chromechat-2d5d5-firebase-adminsdk-248o3-b99106582a.json");
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(21906, '127.0.0.1');
+console.log('Server running');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -16,7 +23,7 @@ ref.once("value", function(snapshot) {
 var usersRef = ref.child("users")
 usersRef.set({
     bruh: {
-        date_of_bith: "June 23, 1912",
+        date_of_birth: "June 23, 1912",
         full_name: "Alan Turing"
     }
 })
