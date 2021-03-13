1e4b6d3612d4c4d494cded247ee736dd9fd45cf4
Tim Beatham <beatham.tim@gmail.com>
2021-03-13 20:44:46 +0000

    Done some stuff.

--- frontend/js/events.js
+++ frontend/js/events.js
@@ -8,8 +8,11 @@ chrome.runtime.onMessage.addListener(
         switch (request.request) {
             case "message":
                 let messageObj = request.payload;
-                box.addMessage(messageObj.username, (new Date()).getDate(), messageObj.message)
+                box.addMessage(messageObj.username, (new Date()).setTime(messageObj.timestamp), messageObj.message)
         }
 
     }
 )
+
+const myURL = "about:blank";
+
