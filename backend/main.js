2996ff3158660ef4c4258a1ae1c8e6e6ed586c73
Tim Beatham <beatham.tim@gmail.com>
2021-03-13 13:30:21 +0000

    Firebase api key

---
+++ backend/main.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB3SW_iZLe-yVxYyT5ZPl0RGXBtb_hqd-A",
+    authDomain: "chromechat-2d5d5.firebaseapp.com",
+    databaseURL: "https://chromechat-2d5d5-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chromechat-2d5d5",
+    storageBucket: "chromechat-2d5d5.appspot.com",
+    messagingSenderId: "627408792415",
+    appId: "1:627408792415:web:aab33f2343fcd94fbba4ea"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
