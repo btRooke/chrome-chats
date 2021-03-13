409b7ddd0f1f02ce13958bada61c9c5821a1e4e4
Tim Beatham <beatham.tim@gmail.com>
2021-03-13 13:51:36 +0000

    Figured firebase out

--- backend/main.js
+++ backend/main.js
@@ -1,12 +1,22 @@
 const admin = require('firebase-admin');
 
-// Your web app's Firebase configuration
-var admin = require("firebase-admin");
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("./chromechat-2d5d5-firebase-adminsdk-248o3-b99106582a.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://chromechat-2d5d5-default-rtdb.europe-west1.firebasedatabase.app"
 });
 
+var db = admin.database();
+var ref = db.ref("/some_resource");
+ref.once("value", function(snapshot) {
+    console.log(snapshot.val());
+})
+
+var usersRef = ref.child("users")
+usersRef.set({
+    bruh: {
+        date_of_bith: "June 23, 1912",
+        full_name: "Alan Turing"
+    }
+})
