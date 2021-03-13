b41ef1f94106f40086b9372c18d050bba67fc884
Tim Beatham <beatham.tim@gmail.com>
2021-03-13 15:25:30 +0000

    Socket io

--- backend/main.js
+++ backend/main.js
@@ -1,19 +1,20 @@
 const admin = require('firebase-admin');
+const app = require('express')();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+const path = require('path');
+
 
 var serviceAccount = require("./chromechat-2d5d5-firebase-adminsdk-248o3-b99106582a.json");
-var http = require('http');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(21906, '127.0.0.1');
-console.log('Server running');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://chromechat-2d5d5-default-rtdb.europe-west1.firebasedatabase.app"
 });
-
+/*  */
 var db = admin.database();
 var ref = db.ref("/some_resource");
 ref.once("value", function(snapshot) {
@@ -27,3 +28,18 @@ usersRef.set({
         full_name: "Alan Turing"
     }
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('temp.html'));
+});
+
+
+io.on('connection', (socket) => {
+    console.log('A user connected!'); 
+});
+
+const PORT = 2000;
+
+http.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+});
