2dbb96843e2ae63827eebb870f343d197fa45748
forkree <moonshot9210@gmail.com>
2021-03-13 23:24:35 +0000

    hook up Chrome options sync, so user's option choices persist
    if you're logged into the Chrome browser, your options should
    be saved and reloaded on your other devices

--- frontend/js/options.js
+++ frontend/js/options.js
@@ -1,5 +1,3 @@
-
-
 document.querySelector("#username-text-box").addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
         console.log(e.target.value.trim());
@@ -9,3 +7,41 @@ document.querySelector("#username-text-box").addEventListener("keypress", (e) =>
 });
 
 chrome.runtime.sendMessage({request: 'get-messages'}, username: )
+
+// https://developer.chrome.com/docs/extensions/mv2/options/
+// Saves options to chrome.storage
+function save_options() {
+    var username = document.getElementById('username-text-box').value;
+    var colour = document.getElementById('colour').value;
+    var pagination = document.getElementById('pagination').value;
+    chrome.storage.sync.set({
+        username: username,
+        colour: colour,
+        pagination: 100
+    }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default value colour = 'Red' and pagination = 100.
+    chrome.storage.sync.get({
+        username: '',
+        colour: 'Red',
+        pagination: 100
+    }, function(items) {
+        document.getElementById('username-text-box').value = items.username;
+        document.getElementById('colour').value = items.colour;
+        document.getElementById('pagination').value = items.pagination;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+                                                 save_options);
