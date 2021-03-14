5457b18f4897c89d1e3dd723ca65210564f8a7f2
Sean Patterson <sap22@st-andrews.ac.uk>
2021-03-14 03:32:58 +0000

    Rollback some more (hopefully temporary)

--- backend/db.js
+++ backend/db.js
@@ -84,68 +84,19 @@ function createDownloadUrl(bucket, pathToFile, downloadToken) {
 }*/
 
 const admin = require('firebase-admin');
-const fetch = require('node-fetch');
-const { v4: uuidv4 } = require('uuid');
-
-const db = admin.database();                // Messages stored here
-const images = admin.storage().bucket();    // Images stored here
+const db = admin.database();
 
 module.exports.getMessages = function(room, io) {
-    db.ref('sites/' + room.hash).child('messages').orderByKey()
+    db.ref("sites/" + room.hash).child('messages').orderByKey()
         .on('child_added', (snapshot) => {
-            if (snapshot.child('imageURL')) {
-                loadImage(snapshot, (image) => {
-                    snapshot.message = image;
-                    delete snapshot.imageURL;
-                    room.messages.push(snapshot);
-                    io.to(room.hash).emit('message', snapshot);
-                });
-            } else {
-                delete snapshot.imageURL;
-                room.messages.push(snapshot);
-                io.to(room.hash).emit('message', snapshot);
-            }
+            room.messages.push(snapshot);
+            io.to(room.hash).emit('message', snapshot);
         }, (error) => console.log(error));
 }
 
 
-module.exports.addMessage = function(hash, message) {
+module.exports.sendMessage = function(url, message) {
     message.timestamp = Date.now();
-    message.imageURL = null;
-
-    db.ref('sites/' + hash).child('messages').push(message);
+    console.log(JSON.stringify(message));
+    db.ref("sites/" + url).child('messages').push(message)
 }
-
-
-module.exports.addImage = function(hash, message) {
-    let doc = db.ref('sites/' + hash).child('messages').push();
-    let token = uuidv4();
-
-    images.upload(message.message, {
-        destination: doc.key,
-        metadata: {
-            cacheControl: "max-age=31536000",
-            metadata: {
-                firebaseStorageDownloadTokens: token
-            }
-        }
-    }).then(() => {
-        message.message = token;
-        message.timestamp = Date.now();
-        message.imageURL = doc.key;
-        doc.set(message).then();
-    }).catch(err => {
-        console.log(err);
-    });
-}
-
-
-function loadImage(doc, cb) {
-    let url = createDownloadUrl(images.name, doc.key, doc.message)
-    fetch(url).then(res => cb(res.blob()));
-}
-
-const createDownloadUrl = (bucket, pathToFile, downloadToken) => {
-    return `https://firebasestorage.googleapis.com/v0/b/${bucket}/o/
-            ${encodeURIComponent(pathToFile)}?alt=media&token=${downloadToken}`;
-};
