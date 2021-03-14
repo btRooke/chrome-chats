4b8921b4c807335edaa520d190eace53df803349
Tim Beatham <beatham.tim@gmail.com>
2021-03-14 04:14:50 +0000

    new branch

--- backend/db.js
+++ backend/db.js
@@ -6,30 +6,23 @@ const db = admin.database();                // Messages stored here
 const images = admin.storage().bucket();    // Images stored here
 
 module.exports.getMessages = function(room, io) {
-    db.ref('sites/' + room.hash).child('messages')
-        .orderByKey()
-        //.limitToLast(number)
+    db.ref('sites/' + room.hash).child('messages').orderByKey()
         .on('child_added', (snapshot) => {
-            loadMessage(snapshot, (doc) => {
-                room.messages.push(doc);
-                //room.loadedMessages++;
-                io.to(room.hash).emit('message', doc);
-            });
+            if (snapshot.child('imageURL')) {
+                loadImage(snapshot, (image) => {
+                    snapshot.message = image;
+                    delete snapshot.imageURL;
+                    room.messages.push(snapshot);
+                    io.to(room.hash).emit('message', snapshot);
+                });
+            } else {
+                delete snapshot.imageURL;
+                room.messages.push(snapshot);
+                io.to(room.hash).emit('message', snapshot);
+            }
         }, (error) => console.log(error));
 }
 
-module.exports.getNextPage = function(room, socket, total, number) {
-    db.ref('sites/' + room.hash).child('messages')
-        .orderByKey()
-        .endBefore(total)
-        .limitToLast(number)
-        .once('child_added', (snapshot) => {
-            loadMessage(snapshot, (doc) => {
-                room.messages.unshift(doc);
-                //room.loadedMessages++;
-            });
-        }, (error) => console.log(error)).then();
-}
 
 module.exports.addMessage = function(hash, message) {
     message.timestamp = Date.now();
@@ -38,6 +31,7 @@ module.exports.addMessage = function(hash, message) {
     db.ref('sites/' + hash).child('messages').push(message);
 }
 
+
 module.exports.addImage = function(hash, message) {
     let doc = db.ref('sites/' + hash).child('messages').push();
     let token = uuidv4();
@@ -45,7 +39,7 @@ module.exports.addImage = function(hash, message) {
     images.upload(message.message, {
         destination: doc.key,
         metadata: {
-            cacheControl: 'max-age=31536000',
+            cacheControl: "max-age=31536000",
             metadata: {
                 firebaseStorageDownloadTokens: token
             }
@@ -60,25 +54,13 @@ module.exports.addImage = function(hash, message) {
     });
 }
 
-function loadMessage(doc, cb) {
-    if (doc.child('imageURL')) {
-        loadImage(doc, (image) => {
-            doc.message = image;
-            delete doc.imageURL;
-            cb(doc);
-        });
-    } else {
-        delete doc.imageURL;
-        cb(doc);
-    }
-}
 
 function loadImage(doc, cb) {
     let url = createDownloadUrl(images.name, doc.key, doc.message)
     fetch(url).then(res => cb(res.blob()));
 }
 
-function createDownloadUrl(bucket, pathToFile, downloadToken) {
+const createDownloadUrl = (bucket, pathToFile, downloadToken) => {
     return `https://firebasestorage.googleapis.com/v0/b/${bucket}/o/
             ${encodeURIComponent(pathToFile)}?alt=media&token=${downloadToken}`;
-}
+};
